9137946b3f4770ca942c7af19baa641bb49364d7
Lucas Mota <contato.lukamota@gmail.com>
2025-05-28 23:31:09 -0300

    feat: add clinic creation form and dialog components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
 import SignOutButton from "./components/sign-out-button";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { usersToClinicsTable } from "@/db/schema";
 
 const DashboardPage = async () => {
    const session = await auth.api.getSession({
@@ -10,6 +13,16 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+    with: {
+      clinic: true,
+    },
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return ( <div>
